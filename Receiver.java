160f0f6adfa4cb06b9961e41d33d54a03b848ae3
anq3 <anq3@gold19.cs.calvin.edu>
2019-12-03 18:41:05 -0500

    Sender breaks file into byte-sized chunks

--- Receiver.java
+++ Receiver.java
@@ -1,51 +1,27 @@
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.io.File;
-import java.nio.file.Files;
-
+import java.io.*;
+import java.net.*;
 
 public class Receiver extends Thread{
-    private DatagramSocket sock;
-    private boolean listening;
-    private byte[] buffer = new byte[1450];
- 
-    private static int portNum;
-    private static String fileName;
-
-    public Receiver(int port, String name) {
-        
-        portNum = port;
-        fileName = name;
-        
-        try{
-            sock = new DatagramSocket(portNum);
-
-            listening = true;
-            while (listening) {
-                try{
-                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                    sock.receive(packet);
-    
-                    String received = new String(packet.getData(), 0, packet.getLength());
-                    System.out.println("got something");
-    
-                }catch(Exception e){
-                    System.out.println(e.toString());
-                }
-    
-            }
-            sock.close();
-
-        }catch(Exception e){
-            System.out.println(e.toString());
+    public static void main(String args[]) throws Exception
+    {
+        DatagramSocket serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
+        byte[] receiveData = new byte[1450];
+        //byte[] sendData = new byte[500];
+        while(true)
+        {
+            //receiveData = new byte[1450];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String( receivePacket.getData());
+            System.out.println("RECEIVED: " + sentence);
+            // return;
+            //InetAddress IPAddress = receivePacket.getAddress();
+            //int port = receivePacket.getPort();
+            // String capitalizedSentence = sentence.toUpperCase();
+            // sendData = capitalizedSentence.getBytes();
+            // DatagramPacket sendPacket =
+            // new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            // serverSocket.send(sendPacket);
         }
-    }
-
-    public static void main(String[] args){
-        portNum = Integer.parseInt(args[0]);
-        fileName = args[1];
-
-        Receiver r = new Receiver(portNum, fileName);
     }
 }
--- Sender.java
+++ Sender.java
@@ -1,54 +1,61 @@
- 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.*;
+import java.net.*;
 import java.io.File;
 import java.nio.file.Files;
 
 public class Sender extends Thread{
-    private DatagramSocket sock;
-    private byte[] buffer = new byte[1450];
-
-    private static String ipAddress;
-    private static int portNum;
-    private static String fileName;
-
-    public Sender(String ip, int port, String file) throws Exception{
-        sock = new DatagramSocket(port);
-        System.out.println(sock.getLocalPort());
-
-        try{
-            //InetAddress address = InetAddress.getByName("localhost");
-            InetAddress address = InetAddress.getLocalHost();
-            System.out.println(address.toString());
-            byte[] info = readFile(file);
-            buffer = info;
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
-            sock.send(packet);
-            sock.close();
-        }catch(Exception e){
-            System.out.println("Something went wrong!");
-            System.out.println(e.toString());
-        }
+    public static void main(String args[]) throws Exception
+    {
+       BufferedReader inFromUser =
+          new BufferedReader(new InputStreamReader(System.in));
+       DatagramSocket clientSocket = new DatagramSocket();
+       InetAddress IPAddress = InetAddress.getByName("localhost");
+       //byte[] sendData = new byte[1450];
+       byte[] receiveData = new byte[1450];
+       String sentence = inFromUser.readLine();
+       //sendData = sentence.getBytes();
+       //sendData = readFile(args[2]);
+       byte[][] allData = biteOff(readFile(args[2]), 1450);
+       for(int i = 0; i < allData.length; i++){
+            DatagramPacket sendPacket = new DatagramPacket(allData[i], allData[i].length, IPAddress, Integer.parseInt(args[1]));
+            clientSocket.send(sendPacket);
+            System.out.print(i);
+       }
+       
+    //    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+    //    clientSocket.receive(receivePacket);
+    //    String modifiedSentence = new String(receivePacket.getData());
+    //    System.out.println("FROM SERVER:" + modifiedSentence);
+       clientSocket.close();
     }
 
-    byte[] readFile(String name) throws Exception{
+    static byte[] readFile(String name) throws Exception{
 
         File file = new File(name);
         byte[] fileContent = Files.readAllBytes(file.toPath());
         return fileContent;
     }
 
-    public static void main(String[] args){
-        ipAddress = args[0];
-        portNum = Integer.parseInt(args[1]);
-        fileName = args[2];
+    static byte[][] biteOff(byte[] input, int size){
+        int originalLength = input.length;
+        int divisions = originalLength / size;
+        byte[][] answer = new byte[divisions][];
 
-        try{
-            Sender s = new Sender(ipAddress, portNum, fileName);
-            s.run();
-        }catch (Exception e){
-            //do nothing
+        for(int i = 0; i < divisions; i++){
+            int startIndex = i * size;
+            int endIndex = (i + 1) * size;
+            if(endIndex > input.length){
+                endIndex = input.length - 1;
+            }
+            int arraySize = endIndex - startIndex;
+
+            answer[i] = new byte[arraySize];
+
+            for(int j = 0; j < arraySize; j++){
+                answer[i][j] = input[i * size + j];
+            }
         }
+
+        return answer;
     }
 }
