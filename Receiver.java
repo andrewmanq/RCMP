171292d79d165d2643a66088aafbf70ce1e67cf8
fun and creative username <hiro@wintermute.localdomain>
2019-12-06 12:50:42 -0500

    added file writing to reciever, made message sending much more reliable

--- Receiver.java
+++ Receiver.java
@@ -1,19 +1,63 @@
 import java.io.*;
 import java.net.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Receiver extends Thread{
+
+    private static DatagramSocket serverSocket;
     public static void main(String args[]) throws Exception
     {
-        DatagramSocket serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
-        byte[] receiveData = new byte[1450];
-        //byte[] sendData = new byte[500];
+        Map<Integer, byte[]> allDataRecieved = new HashMap<Integer, byte[]>();
+        int iterator = 0;
+
+        String filePath = "newFile.txt";
+        File newFile = new File(filePath);
+        try{
+            filePath = args[1];
+        }catch(Exception e){
+            System.out.println("default filename used (newFile.txt)");
+        }
+
+        FileOutputStream fos;
+        try{
+            fos = new FileOutputStream(filePath, true);
+        }catch(Exception e){
+            System.out.println("there was a problem writing out the file.");
+            return;
+        }
+        
+        try{
+            serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
+        }catch(Exception e){
+            System.out.print("Port configuration failed. Did you use a proper port number?");
+            return;
+        }
         while(true)
         {
-            //receiveData = new byte[1450];
+            byte[] receiveData = new byte[1450];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
-            String sentence = new String( receivePacket.getData());
-            System.out.println("RECEIVED: " + sentence);
+            if(receivePacket.getLength() < receiveData.length){
+                byte[] resizedData = new byte[receivePacket.getLength() - 1];
+                System.arraycopy(receiveData, 0, resizedData, 0, receivePacket.getLength() - 1);
+                receiveData = resizedData;
+            }
+            //String sentence = new String( receivePacket.getData());
+            //System.out.println("RECEIVED: " + sentence);
+            
+            allDataRecieved.put(iterator, receiveData);
+
+            try{
+                fos.write(receiveData);
+            }catch(Exception e){
+                System.out.println("there was a problem writing out the file.");
+                return;
+            }
+            //writeFile(filePath, receiveData);
+
             // return;
             //InetAddress IPAddress = receivePacket.getAddress();
             //int port = receivePacket.getPort();
@@ -22,6 +66,17 @@ public class Receiver extends Thread{
             // DatagramPacket sendPacket =
             // new DatagramPacket(sendData, sendData.length, IPAddress, port);
             // serverSocket.send(sendPacket);
+            iterator ++;
+            System.out.println("packet " + iterator + " recieved");
         }
+    }
+
+    private static void writeFile(String pathname, byte[] info){
+        try{
+            FileOutputStream fos = new FileOutputStream(pathname, true);
+            fos.write(info);
+         }catch(Exception e){
+            System.out.println("File output failed.");
+         }
     }
 }
--- Sender.java
+++ Sender.java
@@ -6,27 +6,26 @@ import java.nio.file.Files;
 public class Sender extends Thread{
     public static void main(String args[]) throws Exception
     {
-       BufferedReader inFromUser =
-          new BufferedReader(new InputStreamReader(System.in));
-       DatagramSocket clientSocket = new DatagramSocket();
-       InetAddress IPAddress = InetAddress.getByName("localhost");
-       //byte[] sendData = new byte[1450];
-       byte[] receiveData = new byte[1450];
-       String sentence = inFromUser.readLine();
-       //sendData = sentence.getBytes();
-       //sendData = readFile(args[2]);
-       byte[][] allData = biteOff(readFile(args[2]), 1450);
-       for(int i = 0; i < allData.length; i++){
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        //sendData = sentence.getBytes();
+        //sendData = readFile(args[2]);
+        byte[] rawData = readFile(args[2]);
+        System.out.println("file size: " + rawData.length + " bytes");
+        byte[][] allData = biteOff(rawData, 1450);
+        System.out.println("frames required: " + allData.length);
+        for(int i = 0; i < allData.length; i++){
             DatagramPacket sendPacket = new DatagramPacket(allData[i], allData[i].length, IPAddress, Integer.parseInt(args[1]));
             clientSocket.send(sendPacket);
-            System.out.print(i);
-       }
-       
+            sleep((long)2);
+            //System.out.print(i);
+        }
+        
     //    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
     //    clientSocket.receive(receivePacket);
     //    String modifiedSentence = new String(receivePacket.getData());
     //    System.out.println("FROM SERVER:" + modifiedSentence);
-       clientSocket.close();
+        clientSocket.close();
     }
 
     static byte[] readFile(String name) throws Exception{
@@ -36,14 +35,14 @@ public class Sender extends Thread{
         return fileContent;
     }
 
-    static byte[][] biteOff(byte[] input, int size){
+    static byte[][] biteOff(byte[] input, int biteSize){
         int originalLength = input.length;
-        int divisions = (int)(Math.ceil((double)originalLength / (double)size));
+        int divisions = (int)(Math.ceil((double)originalLength / (double)biteSize));
         byte[][] answer = new byte[divisions][];
 
         for(int i = 0; i < divisions; i++){
-            int startIndex = i * size;
-            int endIndex = (i + 1) * size;
+            int startIndex = i * biteSize;
+            int endIndex = (i + 1) * biteSize;
             if(endIndex > input.length){
                 endIndex = input.length - 1;
             }
@@ -52,7 +51,7 @@ public class Sender extends Thread{
             answer[i] = new byte[arraySize];
 
             for(int j = 0; j < arraySize; j++){
-                answer[i][j] = input[i * size + j];
+                answer[i][j] = input[i * biteSize + j];
             }
         }
 
