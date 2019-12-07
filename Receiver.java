ae23bd3465781603ab1538cd59559dedf320cae5
anq3 <anq3@maroon01.cs.calvin.edu>
2019-12-07 17:50:51 -0500

    Finished all requirements for RCMP to be the dominant protocol. Time to rake in the cash 7032

--- Receiver.java
+++ Receiver.java
@@ -6,19 +6,48 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
+
+//  ______    _______  _______  _______  ___   __   __  _______  ______              ___  _______  __   __  _______ 
+// |    _ |  |       ||       ||       ||   | |  | |  ||       ||    _ |            |   ||   _   ||  | |  ||   _   |
+// |   | ||  |    ___||       ||    ___||   | |  |_|  ||    ___||   | ||            |   ||  |_|  ||  |_|  ||  |_|  |
+// |   |_||_ |   |___ |       ||   |___ |   | |       ||   |___ |   |_||_           |   ||       ||       ||       |
+// |    __  ||    ___||      _||    ___||   | |       ||    ___||    __  | ___   ___|   ||       ||       ||       |
+// |   |  | ||   |___ |     |_ |   |___ |   |  |     | |   |___ |   |  | ||   | |       ||   _   | |     | |   _   |
+// |___|  |_||_______||_______||_______||___|   |___|  |_______||___|  |_||___| |_______||__| |__|  |___|  |__| |__|
+/**
+ * @author  Andrew Quist, Sambridhi Acharya
+ * created on Dec 7, 2019
+ * 
+ * Receiver.java is a program that utilizes UDP to make a new internet protocol -- RCMP
+ * RCMP is a protocol that is both message-based and reliable.
+ * 
+ * Receiver.java receives packets and sends ACK messages for the Sender to continue sending.
+ * 
+ * HOW TO USE:
+ * argument 0 - port number that the file will be received on
+ * argument 1 - the name of the file that will be written out to the disk (in the current directory)
+ */
 public class Receiver extends Thread{
 
+    //Records the ID of the sender
     private static byte[] id;
+    //Records the total packet number we will receive
     private static int fullSize = 0;
 
+    //Socket that we get data from
     private static DatagramSocket serverSocket;
+    //File output for writing the received file
+    private static FileOutputStream fos;
+
+    //This constant is the data portion of our RCMP packet.
+    private static final int DATASIZE = 1450;
+    private static final int HEADERSIZE = 13;
     public static void main(String args[]) throws Exception
     {
-
-        Map<Integer, byte[]> allDataRecieved = new HashMap<Integer, byte[]>();
-
         //PARSE ARGUMENTS----------------------------------------
+        //filePath defualt
         String filePath = "newFile.txt";
         File newFile = new File(filePath);
         try{
@@ -28,7 +57,7 @@ public class Receiver extends Thread{
         }
 
         //FILE OPEN----------------------------------------
-        FileOutputStream fos;
+        //Try to make the new file with file path
         try{
             fos = new FileOutputStream(filePath, true);
         }catch(Exception e){
@@ -38,101 +67,169 @@ public class Receiver extends Thread{
 
         //OPEN SOCKET----------------------------------------
         try{
+            //Open a socket with the first argument (the port number)
             serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
         }catch(Exception e){
             System.out.print("Port configuration failed. Did you use a proper port number?");
+            fos.close();
             return;
         }
 
         System.out.println("Recieving file...");
 
-        //INITIALIZE NON-LOOP VARIABLES----------------------------------------
+        //INITIALIZE LOOP-DEPENDANT VARIABLES----------------------------------------
         id = new byte[4];
         fullSize = 0;
 
+        //Records the last received packet
+        int previousPacket = -1;
+        //Counts how many messages it has received
         int iterator = 0;
+
         while(true)
         {
-
-            byte[] receiveData = new byte[1450 + 13];
+            //Get data from socket
+            byte[] receiveData = new byte[DATASIZE + HEADERSIZE];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
 
+            //valid is a simple way of determining whether we want to drop or process the packet
             boolean valid = true;
 
             //IF FIRST PACKET----------------------------------------
             if(iterator == 0){
-                id[0] = receiveData[0]; id[1] = receiveData[1];
-                id[2] = receiveData[2]; id[3] = receiveData[3];
+                //Parses the ID number from the first incoming packet
+                assignID(receiveData);
+                System.out.println("ID: " + id.toString());
 
-                byte[] sizeIndicator = new byte[4];
-                sizeIndicator[0] = receiveData[4]; sizeIndicator[1] = receiveData[5];
-                sizeIndicator[2] = receiveData[6]; sizeIndicator[3] = receiveData[7];
-                fullSize = Integer.parseInt(new String(sizeIndicator));
-
+                //Parses the total packet number from our first packet
+                assignSize(receiveData);
                 System.out.println("Amount of packets required: " + fullSize);
 
-                System.out.println("ID: " + id.toString());
             //ALL OTHER PACKETS----------------------------------------
             }else{
-                if(receiveData[0] != id[0]){ valid = false; }
-                if(receiveData[1] != id[1]){ valid = false; }
-                if(receiveData[2] != id[2]){ valid = false; }
-                if(receiveData[3] != id[3]){ valid = false; }
+                //Is this the right ID? if not, drop
+                valid = isValidID(receiveData);
             }
 
             //NUMDATA = number of all incoming packets------------------------
-            byte[] numData = new byte[4];
-            numData[0] = receiveData[8]; numData[1] = receiveData[9];
-            numData[2] = receiveData[10]; numData[3] = receiveData[11];
-            int packetNum = Integer.parseInt(new String(numData));
+            int packetNum = extractPacketNumber(receiveData);
+
+            //Is this a future packet? DROP!
+            if(packetNum > previousPacket + 1){
+                valid = false;
+                System.out.println("packet lost");
+            }
 
             //IF ID IS FROM THE RIGHT SENDER----------------------------------------
             if(valid){
 
+                //Looks for an ACK request
                 if(receiveData[12] == 1){
                     //FIND SENDER----------------------------------------
                     InetAddress IPAddress = receivePacket.getAddress();
                     int port = receivePacket.getPort();
 
-                    //CREATE ACK-------------------------------------------
-                    byte[] sendData = createAck(packetNum);
-                    DatagramPacket sendPacket =
-                    new DatagramPacket(sendData, sendData.length, IPAddress, port);
-                    serverSocket.send(sendPacket);
+                    //CREATE and SEND ACK-------------------------------------------
+                    sendAck(packetNum, IPAddress, port, serverSocket);
                 }
 
                 //ADJUSTS SIZE----------------------------------------
+                //This looks to see if the packet is smaller than the max packet size
                 if(receivePacket.getLength() < receiveData.length){
-                    byte[] resizedData = new byte[receivePacket.getLength() - 1];
-                    System.arraycopy(receiveData, 0, resizedData, 0, receivePacket.getLength() - 1);
+                    byte[] resizedData = new byte[receivePacket.getLength()];
+                    //Copy all data to the new smaller array
+                    System.arraycopy(receiveData, 0, resizedData, 0, receivePacket.getLength());
                     receiveData = resizedData;
                 }
-                
-                allDataRecieved.put(iterator, receiveData);
 
                 //WRITE FILE----------------------------------------
-                try{
-                    byte[] strippedData = new byte[receiveData.length - 13];
-                    System.arraycopy(receiveData, 13, strippedData, 0, receiveData.length - 13);
-                    fos.write(strippedData);
-                }catch(Exception e){
-                    System.out.println("there was a problem writing out the file.");
-                    return;
+
+                //On the offchance that this is the next packet in the sequence...
+                if(packetNum == previousPacket + 1){
+                    //Write that data
+                    if(writeToFile(receiveData)){
+                        System.out.print("| " + packetNum + " |");
+                    }
+                    //Records the last successful packet sent in order
+                    previousPacket = packetNum;
+
+                }else{
+                    System.out.println("old packet recieved");
                 }
-
                 iterator ++;
-                System.out.print("| packet " + packetNum + " recieved |");
 
+                //Is this the last packet? cool
                 if(packetNum == fullSize - 1){
+                    //Cleanup time
                     fos.close();
                     System.out.println("File recieved.");
+                    //Close the program
                     return;
                 }
             }
         }
     }
 
+    //This takes byte data and writes it to the currently open file (and strips off the header)
+    //Returns true if write is successful
+    private static Boolean writeToFile(byte[] receiveData) throws Exception{
+        try{
+            //Extract payload
+            byte[] strippedData = new byte[receiveData.length - HEADERSIZE];
+            System.arraycopy(receiveData, HEADERSIZE, strippedData, 0, receiveData.length - HEADERSIZE);
+            //Write bytes
+            fos.write(strippedData);
+        }catch(Exception e){
+            System.out.println("there was a problem writing out the file.");
+            fos.close();
+            return false;
+        }
+        return true;
+    }
+
+    //Takes the packet number, address, port, and socket, then sends an ack with packetNum response
+    private static void sendAck(int packetNum, InetAddress IPAddress, int port, DatagramSocket serverSocket) throws Exception{
+        byte[] sendData = createAck(packetNum);
+        DatagramPacket sendPacket =
+            new DatagramPacket(sendData, sendData.length, IPAddress, port);
+        serverSocket.send(sendPacket);
+    }
+
+    //Parses the packet number from the datagram header
+    private static int extractPacketNumber(byte[] receiveData){
+        byte[] numData = new byte[4];
+        numData[0] = receiveData[8]; numData[1] = receiveData[9];
+        numData[2] = receiveData[10]; numData[3] = receiveData[11];
+        int packetNum = Integer.parseInt(new String(numData));
+        return packetNum;
+    }
+
+    //Makes sure the packet ID is correct
+    private static Boolean isValidID(byte[] receiveData){
+        Boolean valid = true;
+        if(receiveData[0] != id[0]){ valid = false; }
+        if(receiveData[1] != id[1]){ valid = false; }
+        if(receiveData[2] != id[2]){ valid = false; }
+        if(receiveData[3] != id[3]){ valid = false; }
+        return valid;
+    }
+
+    //Extracts the number of packets we will recieve, puts it in private variable fullSize
+    private static void assignSize(byte[] receiveData){
+        byte[] sizeIndicator = new byte[4];
+        sizeIndicator[0] = receiveData[4]; sizeIndicator[1] = receiveData[5];
+        sizeIndicator[2] = receiveData[6]; sizeIndicator[3] = receiveData[7];
+        fullSize = Integer.parseInt(new String(sizeIndicator));
+    }
+
+    //Fills ID private variable with the packet ID header field
+    private static void assignID(byte[] receiveData){
+        id[0] = receiveData[0]; id[1] = receiveData[1];
+        id[2] = receiveData[2]; id[3] = receiveData[3];
+    }
+
+    //Quickly cooks up an ACK request in a byte array to be sent back to the sender
     private static byte[] createAck(int packetNum){
         byte[] ack = new byte[8];
 
@@ -151,15 +248,16 @@ public class Receiver extends Thread{
         return ack;
     }
 
-    private static void writeFile(String pathname, byte[] info){
-        try{
-            FileOutputStream fos = new FileOutputStream(pathname, true);
-            fos.write(info);
-         }catch(Exception e){
-            System.out.println("File output failed.");
-         }
-    }
+    // private static void writeFile(String pathname, byte[] info){
+    //     try{
+    //         FileOutputStream fos = new FileOutputStream(pathname, true);
+    //         fos.write(info);
+    //      }catch(Exception e){
+    //         System.out.println("File output failed.");
+    //      }
+    // }
 
+    //A nice function to convert an int to a 4-byte array
     private static byte[] get4ByteInt(int num){
         String info = Integer.toString(num);
         while(info.getBytes().length < 4){
--- Sender.java
+++ Sender.java
@@ -5,61 +5,188 @@ import java.nio.file.Files;
 import java.util.Random;
 import java.nio.ByteBuffer;
 
+// _______  _______  __    _  ______   _______  ______              ___  _______  __   __  _______ 
+// |       ||       ||  |  | ||      | |       ||    _ |            |   ||   _   ||  | |  ||   _   |
+// |  _____||    ___||   |_| ||  _    ||    ___||   | ||            |   ||  |_|  ||  |_|  ||  |_|  |
+// | |_____ |   |___ |       || | |   ||   |___ |   |_||_           |   ||       ||       ||       |
+// |_____  ||    ___||  _    || |_|   ||    ___||    __  | ___   ___|   ||       ||       ||       |
+//  _____| ||   |___ | | |   ||       ||   |___ |   |  | ||   | |       ||   _   | |     | |   _   |
+// |_______||_______||_|  |__||______| |_______||___|  |_||___| |_______||__| |__|  |___|  |__| |__|
+/**
+ * @author  Andrew Quist, Sambridhi Acharya
+ * created on Dec 7, 2019
+ * 
+ * Sender.java is a program that utilizes UDP to make a new internet protocol -- RCMP
+ * RCMP is a protocol that is both message-based and reliable.
+ * 
+ * Sender sends packets and receives ACK messages to acknowledge that the packets have sent.
+ * A Receiver object must be opened at the corresponding IP and port (given in arguments)
+ * 
+ * HOW TO USE:
+ * argument 0 - host address (can be host name OR an IP address)
+ * argument 1 - port number of the receiver program
+ * argument 2 - the name of the file that will be sent
+ */
 public class Sender extends Thread{
 
+    //Colors for nice formatting on the console output
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RESET = "\u001B[0m";
 
+    //The socket that we will send data over
+    private static DatagramSocket clientSocket;
+
+    //id is the unique id of this sender. It is used so that the receiver knows which sender it is
+    //receiving this file from.
     private static byte[] id;
+    //msgSz (message size for short) is the total amount of packets to be sent over the network.
     private static byte[] msgSz;
+
+    //This constant is the data portion of our RCMP packet.
+    private static final int DATASIZE = 1450;
+
+    private static final int TIMEOUT = 50;
     public static void main(String args[]) throws Exception
     {   
+        //This generates a unique id for this sender (4 bytes)
         id = getRandomID();
+
+        //Initialize the address we will send to with the first argument
+        InetAddress IPAddress;
+        String ip = args[0];
+        IPAddress = InetAddress.getByName(ip);
+
+        //Open the socket we will send data over
+        try{
+            clientSocket = new DatagramSocket();
+        }catch(Exception e){
+            System.out.println("There was a problem opening an internet connection. quitting...");
+            return;
+        }
+        
+        //allData is the file contents loaded into the RAM.
+        //the data is loaded into chunks that are ready to be sent in a datagram.
+        byte[][] allData;
+
+        //This will read the file that we will open
+        try{
+            //Read in all the data and process it
+            byte[] rawData = readFile(args[2]);
+            //tell the user how big the file is
+            System.out.println("file size: " + rawData.length + " bytes");
+            //Process the data
+            allData = biteOff(rawData, DATASIZE);
+        }catch(Exception e){
+            System.out.println("There was a problem reading and processing data. quitting...");
+            return;
+        }
+
+        //gapCount is increased when ACKs are successfully delivered.
         int gapCount = 0;
+        //This records when we will send another ACK
         int nextExpectedAck = 0;
-
-        DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress IPAddress = InetAddress.getByName("localhost");
-        byte[] rawData = readFile(args[2]);
-        System.out.println("file size: " + rawData.length + " bytes");
-
-        byte[][] allData = biteOff(rawData, 1450);
+        //This is the packet that was last designated to send an ACK
+        int lastReceivedAck = 0;
+        //This is incremented when an ACK is not received at the last packet.
+        int lastAckIncomplete = 0;
 
         System.out.println("frames required: " + allData.length);
+
+        //Iterates through all the data to send over the network.
+        //i is always the current datagram getting sent
         for(int i = 0; i < allData.length; i++){
 
+            //Checks if this datagram needs and ack request
             if(nextExpectedAck == i){
                 insertAckRequest(allData[i]);
             }
 
-            DatagramPacket sendPacket = new DatagramPacket(allData[i], allData[i].length, IPAddress, Integer.parseInt(args[1]));
+            //Creates a new packet to be sent
+            DatagramPacket sendPacket = new DatagramPacket(allData[i],
+                allData[i].length, IPAddress, Integer.parseInt(args[1]));
+            //sends datagram
             clientSocket.send(sendPacket);
+            System.out.print(i + ".");
             //System.out.print(i);
 
+            //Runs when the sender requires an ACK
             if(nextExpectedAck == i){
+
+                //ackNum is what we will use to record the ack number received.
+                int ackNum;
+
+                //Timeout for the ack request
+                clientSocket.setSoTimeout(TIMEOUT);
+
+                //Will contain the data from the ACK
                 byte[] receiveData = new byte[8];
-                while(parseACKvalid(receiveData) == false){
+
+                //correctACK is used to detect if the ACK is the ACK we are ACKtually looking for
+                Boolean correctACK = false;
+
+                //Loops if the ACK is not the one we're looking for
+                while(!correctACK){
+                    //The incoming packet information
                     DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                    clientSocket.receive(receivePacket);
+
+                    //gets an ACK, sees if it's the number of this packet
+                    try{
+                        clientSocket.receive(receivePacket);
+                        ackNum = parseACKnumber(receiveData);
+
+                        //Exits ACK loop
+                        if(parseACKvalid(receiveData) == true && ackNum == i){
+                            correctACK = true;
+                        }
+
+                    //If the ACK request times out
+                    }catch (SocketTimeoutException e){
+                        System.out.print("NO ACK RECIEVED!");
+
+                        //Failsafe for if the ack was dropped for the end of the file and
+                        //the receiver has closed
+                        if(i == allData.length - 1){
+                            lastAckIncomplete ++;
+
+                            //If the ack was ignored 5 times, just give up
+                            if(lastAckIncomplete >= 5){
+                                System.out.println("File transfer success unknown.");
+                                clientSocket.close();
+                                return;
+                            }
+                        }
+
+                        //Go back to the old ACK checkpoint because there was a packet dropped
+                        i = lastReceivedAck - 1;
+
+                        //Resets the gap count (unreliable network)
+                        gapCount = 0;
+
+                        break;
+                    }
+
+                    if(correctACK){
+                        //ACK has been received correctly, record this ACK checkpoint
+                        System.out.println(ANSI_GREEN + "ack " + ackNum + " recieved" + ANSI_RESET);
+                        lastReceivedAck = i;
+                    }
                 }
-                int ackNum = parseACKnumber(receiveData);
 
-                System.out.println(ANSI_GREEN + "ack " + ackNum + " recieved" + ANSI_RESET);
-
+                //Gap count increases because the ACK was successfully received
                 gapCount ++;
-                nextExpectedAck += gapCount;
+                nextExpectedAck = lastReceivedAck + gapCount;
+                //This sets the last datagram as an ACK
+                if(nextExpectedAck >= allData.length){
+                    nextExpectedAck = allData.length - 1;
+                }
             }
-
-            System.out.print(i + ".");
         }
-        
-        
 
-    //    String modifiedSentence = new String(receivePacket.getData());
-    //    System.out.println("FROM SERVER:" + modifiedSentence);
         clientSocket.close();
     }
 
+    //Makes sure the ACK info is matching my ID
+    //pass in byte[] information (the ACK packet bytes)
     static Boolean parseACKvalid(byte[] information){
         Boolean answer = true;
         if(information[0] != id[0]){ answer = false; }
@@ -69,6 +196,7 @@ public class Sender extends Thread{
         return answer;
     }
     
+    //Returns the ACK packet number from an ACK byte[] array
     static int parseACKnumber(byte[] information){
         int answer = 0;
 
@@ -80,6 +208,7 @@ public class Sender extends Thread{
         return answer;
     }
 
+    //reads all of the data in a file
     static byte[] readFile(String name) throws Exception{
 
         File file = new File(name);
@@ -87,40 +216,54 @@ public class Sender extends Thread{
         return fileContent;
     }
 
+    //Takes a ton of data and cuts it into bytes that are biteSize length, and inserts the packet's header
     static byte[][] biteOff(byte[] input, int biteSize){
         int originalLength = input.length;
+        //How many packets needed to send the data
         int divisions = (int)(Math.ceil((double)originalLength / (double)biteSize));
 
+        //msgSz will be put in the header to say how many packets to expect
         msgSz = get4ByteInt(divisions);
+
+        //HEADER FORMAT: |ID - 4 bytes | Number of packets - 4 bytes | Packet Number - 4 bytes | ACK request - 1 byte |
+        // (payload is after header, is 1450)
 
         byte[][] answer = new byte[divisions][];
 
+        //Goes through each byte and copies it to a packet
         for(int i = 0; i < divisions; i++){
             int startIndex = i * biteSize;
             int endIndex = (i + 1) * biteSize;
+            //The last packet has less data
             if(endIndex > input.length){
-                endIndex = input.length - 1;
+                endIndex = input.length;
             }
-            int arraySize = endIndex - startIndex;
 
+            //Calculates how large the packet is
+            int arraySize = endIndex - startIndex;
             answer[i] = new byte[arraySize + 13];
 
+            //Inserts ID
             for(int j = 0; j < 4; j++){
                 answer[i][j] = id[j];
             }
 
+            //Inserts total packet number
             for(int j = 0; j < 4; j++){
                 answer[i][j + 4] = msgSz[j];
             }
 
+            //Inserts current packet number
             byte[] packetNum = get4ByteInt(i);
 
             for(int j = 0; j < 4; j++){
                 answer[i][j + 8] = packetNum[j];
             }
 
+            //Default ACK is 0, ACK requests are inserted while sending
             answer[i][12] = 0;
 
+            //Fills the payload
             for(int j = 0; j < arraySize; j++){
                 answer[i][j + 13] = input[i * biteSize + j];
             }
@@ -129,12 +272,13 @@ public class Sender extends Thread{
         return answer;
     }
 
+    //This goes in and inserts a '1' byte into the ack field of byte[] data
     private static byte[] insertAckRequest(byte[] data){
         data[12] = 1;
         return data;
     }
 
-    //Returns a random 4-bit id number
+    //Returns a random 4-byte id number
     private static byte[] getRandomID(){
         String idInfo = "";
         Random r = new Random();
@@ -144,6 +288,7 @@ public class Sender extends Thread{
         return idInfo.getBytes();
     }
 
+    //Converts an integer into a 4-byte minimum byte array
     private static byte[] get4ByteInt(int num){
         String info = Integer.toString(num);
         while(info.getBytes().length < 4){
