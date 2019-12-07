4e67c44b68088f3687eb3afe0b0dfecf4d817dd8
anq3 <anq3@maroon11.cs.calvin.edu>
2019-12-06 19:13:59 -0500

    Finished step 2 - Added headers to send the recipient information

--- Receiver.java
+++ Receiver.java
@@ -1,7 +1,9 @@
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,8 +12,10 @@ public class Receiver extends Thread{
     private static DatagramSocket serverSocket;
     public static void main(String args[]) throws Exception
     {
+        byte[] id = new byte[4];
+        int fullSize;
+
         Map<Integer, byte[]> allDataRecieved = new HashMap<Integer, byte[]>();
-        int iterator = 0;
 
         String filePath = "newFile.txt";
         File newFile = new File(filePath);
@@ -35,39 +39,76 @@ public class Receiver extends Thread{
             System.out.print("Port configuration failed. Did you use a proper port number?");
             return;
         }
+
+        System.out.println("Recieving file...");
+
+        int iterator = 0;
         while(true)
         {
-            byte[] receiveData = new byte[1450];
+            
+            byte[] receiveData = new byte[1450 + 12];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
-            if(receivePacket.getLength() < receiveData.length){
-                byte[] resizedData = new byte[receivePacket.getLength() - 1];
-                System.arraycopy(receiveData, 0, resizedData, 0, receivePacket.getLength() - 1);
-                receiveData = resizedData;
-            }
-            //String sentence = new String( receivePacket.getData());
-            //System.out.println("RECEIVED: " + sentence);
-            
-            allDataRecieved.put(iterator, receiveData);
 
-            try{
-                fos.write(receiveData);
-            }catch(Exception e){
-                System.out.println("there was a problem writing out the file.");
-                return;
-            }
-            //writeFile(filePath, receiveData);
+            boolean valid = true;
+            if(iterator == 0){
+                id[0] = receiveData[0]; id[1] = receiveData[1];
+                id[2] = receiveData[2]; id[3] = receiveData[3];
 
-            // return;
-            //InetAddress IPAddress = receivePacket.getAddress();
-            //int port = receivePacket.getPort();
-            // String capitalizedSentence = sentence.toUpperCase();
-            // sendData = capitalizedSentence.getBytes();
-            // DatagramPacket sendPacket =
-            // new DatagramPacket(sendData, sendData.length, IPAddress, port);
-            // serverSocket.send(sendPacket);
-            iterator ++;
-            System.out.println("packet " + iterator + " recieved");
+                byte[] sizeIndicator = new byte[4];
+                sizeIndicator[0] = receiveData[4]; sizeIndicator[1] = receiveData[5];
+                sizeIndicator[2] = receiveData[6]; sizeIndicator[3] = receiveData[7];
+                fullSize = Integer.parseInt(new String(sizeIndicator));
+
+                System.out.println("Amount of packets required: " + fullSize);
+
+                System.out.println("ID: " + id.toString());
+            }else{
+                if(receiveData[0] != id[0]){ valid = false; }
+                if(receiveData[1] != id[1]){ valid = false; }
+                if(receiveData[2] != id[2]){ valid = false; }
+                if(receiveData[3] != id[3]){ valid = false; }
+            }
+
+            byte[] numData = new byte[4];
+            numData[0] = receiveData[8]; numData[1] = receiveData[9];
+            numData[2] = receiveData[10]; numData[3] = receiveData[11];
+            int packetNum = Integer.parseInt(new String(numData));
+
+            if(valid){
+                InetAddress IPAddress = receivePacket.getAddress();
+                int port = receivePacket.getPort();
+                // String capitalizedSentence = sentence.toUpperCase();
+                byte[] sendData = "ACK".getBytes();
+                DatagramPacket sendPacket =
+                new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                serverSocket.send(sendPacket);
+
+                if(receivePacket.getLength() < receiveData.length){
+                    byte[] resizedData = new byte[receivePacket.getLength() - 1];
+                    System.arraycopy(receiveData, 0, resizedData, 0, receivePacket.getLength() - 1);
+                    receiveData = resizedData;
+                }
+                //String sentence = new String( receivePacket.getData());
+                //System.out.println("RECEIVED: " + sentence);
+                
+                allDataRecieved.put(iterator, receiveData);
+
+                try{
+                    fos.write(receiveData);
+                    if(receiveData.length < 1450 + 12){
+                        fos.close();
+                        System.out.println("File recieved.");
+                        return;
+                    }
+                }catch(Exception e){
+                    System.out.println("there was a problem writing out the file.");
+                    return;
+                }
+
+                iterator ++;
+                System.out.println("packet " + packetNum + " recieved");
+            }
         }
     }
 
--- Sender.java
+++ Sender.java
@@ -2,27 +2,38 @@ import java.io.*;
 import java.net.*;
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Random;
+import java.nio.ByteBuffer;
 
 public class Sender extends Thread{
+
+    private static byte[] id;
+    private static byte[] msgSz;
     public static void main(String args[]) throws Exception
-    {
+    {   
+        id = getRandomID();
+        
         DatagramSocket clientSocket = new DatagramSocket();
         InetAddress IPAddress = InetAddress.getByName("localhost");
-        //sendData = sentence.getBytes();
-        //sendData = readFile(args[2]);
         byte[] rawData = readFile(args[2]);
         System.out.println("file size: " + rawData.length + " bytes");
+
         byte[][] allData = biteOff(rawData, 1450);
+
         System.out.println("frames required: " + allData.length);
         for(int i = 0; i < allData.length; i++){
             DatagramPacket sendPacket = new DatagramPacket(allData[i], allData[i].length, IPAddress, Integer.parseInt(args[1]));
             clientSocket.send(sendPacket);
-            sleep((long)2);
             //System.out.print(i);
+
+            byte[] receiveData = new byte["ACK".getBytes().length];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+            System.out.print("_ACK_");
         }
         
-    //    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-    //    clientSocket.receive(receivePacket);
+        
+
     //    String modifiedSentence = new String(receivePacket.getData());
     //    System.out.println("FROM SERVER:" + modifiedSentence);
         clientSocket.close();
@@ -38,6 +49,9 @@ public class Sender extends Thread{
     static byte[][] biteOff(byte[] input, int biteSize){
         int originalLength = input.length;
         int divisions = (int)(Math.ceil((double)originalLength / (double)biteSize));
+
+        msgSz = get4ByteInt(divisions);
+
         byte[][] answer = new byte[divisions][];
 
         for(int i = 0; i < divisions; i++){
@@ -48,13 +62,45 @@ public class Sender extends Thread{
             }
             int arraySize = endIndex - startIndex;
 
-            answer[i] = new byte[arraySize];
+            answer[i] = new byte[arraySize + 12];
+
+            for(int j = 0; j < 4; j++){
+                answer[i][j] = id[j];
+            }
+
+            for(int j = 0; j < 4; j++){
+                answer[i][j + 4] = msgSz[j];
+            }
+
+            byte[] packetNum = get4ByteInt(i);
+            
+            for(int j = 0; j < 4; j++){
+                answer[i][j + 8] = packetNum[j];
+            }
 
             for(int j = 0; j < arraySize; j++){
-                answer[i][j] = input[i * biteSize + j];
+                answer[i][j + 12] = input[i * biteSize + j];
             }
         }
 
         return answer;
+    }
+
+    //Returns a random 4-bit id number
+    private static byte[] getRandomID(){
+        String idInfo = "";
+        Random r = new Random();
+        while(idInfo.getBytes().length < 4){
+            idInfo += r.nextInt(9);
+        }
+        return idInfo.getBytes();
+    }
+
+    private static byte[] get4ByteInt(int num){
+        String info = Integer.toString(num);
+        while(info.getBytes().length < 4){
+            info = "0" + info;
+        }
+        return info.getBytes();
     }
 }
